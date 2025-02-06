f71e60b1332aec6bad0b42cf70e174c113e1f67d
grinnds <40234162+grinnds@users.noreply.github.com>
2025-02-06 22:31:12 +0300

    Add search diagnostics via telescope

--- modules/home_manager/nvim/config/plugin/telescope.lua
+++ modules/home_manager/nvim/config/plugin/telescope.lua
@@ -25,10 +25,13 @@ require("telescope").setup({
 
 require("telescope").load_extension("fzf")
 
-vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
-vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
-vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers)
-vim.keymap.set("n", "<leader>fs", require("telescope.builtin").live_grep)
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files)
+vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+vim.keymap.set("n", "<leader>fb", builtin.buffers)
+vim.keymap.set("n", "<leader>fs", builtin.live_grep)
+vim.keymap.set("n", "<leader>fd", builtin.diagnostics)
 vim.keymap.set("n", "<leader>ec", function()
 	require("telescope.builtin").find_files({
 		cwd = "/etc/nixos/",

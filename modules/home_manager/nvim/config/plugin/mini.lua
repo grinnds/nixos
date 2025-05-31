52820f2537f4ed828a1ac8df1e2948ea3c789962
grinnds <40234162+grinnds@users.noreply.github.com>
2025-05-31 18:23:34 +0300

    Fix mini pairs for rust lifetimes

--- modules/home_manager/nvim/config/plugin/mini.lua
+++ modules/home_manager/nvim/config/plugin/mini.lua
@@ -3,6 +3,13 @@ require("mini.ai").setup({ n_lines = 500 })
 require("mini.surround").setup()
 
 require("mini.pairs").setup()
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	group = vim.api.nvim_create_augroup("custom_rust_disable_single_quote_pairs", { clear = true }),
+	pattern = "rust",
+	callback = function()
+		vim.keymap.set("i", "'", "'", { buffer = 0 })
+	end,
+})
 
 require("mini.icons").setup()
 
